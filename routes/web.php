183a3e69b66526e00e77330ee9ef9fb2f356e398
antum21 <ahmadantum03@gmail.com>
2026-05-19 02:07:40 +0800

    percobaan menggunakan tailwind

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 Route::get('/index', function () {
     return view('index');
+});
+Route::get('/', function () {
+    return view('klikidn');
 });
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import { bunny } from 'laravel-vite-plugin/fonts';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import { bunny } from "laravel-vite-plugin/fonts";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
             fonts: [
-                bunny('Instrument Sans', {
+                bunny("Instrument Sans", {
                     weights: [400, 500, 600],
                 }),
             ],
@@ -17,8 +17,13 @@ export default defineConfig({
         tailwindcss(),
     ],
     server: {
+        host: "0.0.0.0", // Mengizinkan Vite diakses dari luar container Docker
+        port: 5173, // Memastikan port tetap di 5173
+        hmr: {
+            host: "localhost", // Memastikan browser laptop Anda bisa terhubung untuk Hot Reload
+        },
         watch: {
-            ignored: ['**/storage/framework/views/**'],
+            ignored: ["**/storage/framework/views/**"],
         },
     },
 });
